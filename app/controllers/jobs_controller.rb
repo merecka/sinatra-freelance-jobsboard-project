4610670de56eb6b48e885c7e84a3db3a259a60ca
Alex Merecka <merecka@gmail.com>
2019-07-09 03:28:50 -0500

    Fixed job_hours not saving and updating properly

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -27,8 +27,8 @@ class JobsController < ApplicationController
 	end
 
 	post '/jobs/new' do
-		if Helpers.is_logged_in?(session) && session[:owner_id] != nil 
-			if params[:title] != "" && params[:job_hours] != "" && params[:job_description] != "" && params[:salary] != ""
+		if Helpers.is_logged_in?(session) && session[:owner_id] != nil
+			if params[:title] != "" && params[:job_hours] != nil && params[:job_description] != "" && params[:salary] != ""
 				@job = Job.create(title: params[:title], job_hours: params[:job_hours], job_description: params[:job_description], salary: params[:salary])
 				@owner = Helpers.current_user(session)
 				@job.owner = @owner
@@ -39,6 +39,35 @@ class JobsController < ApplicationController
 			end
 		else
 			redirect '/owners/login'
+		end
+	end
+
+	get '/jobs/:id/edit' do
+		if Helpers.is_logged_in?(session) && session[:owner_id] != nil
+			@job = Job.find_by(id: params[:id])
+			@owner = Owner.find_by(id: session[:owner_id])
+			if @job.owner = @owner
+				erb :'/jobs/edit'
+			else
+				redirect "/owners/#{@owner.id}"
+			end
+		else
+			redirect '/'
+		end
+	end
+
+	patch '/jobs/:id/edit' do
+		if Helpers.is_logged_in?(session) && session[:owner_id] != nil
+			@job = Job.find_by(id: params[:id])
+			@owner = Owner.find_by(id: session[:owner_id])
+			if params[:title] != "" && params[:job_hours] != nil && params[:job_description] != "" && params[:salary] != ""
+				@job.update(title: params[:title], job_hours: params[:job_hours], job_description: params[:job_description], salary: params[:salary])
+				redirect "/owners/#{@owner.id}"
+			else
+				redirect "/jobs/#{@job.id}/edit"
+			end
+		else
+			redirect '/'
 		end
 	end
 
